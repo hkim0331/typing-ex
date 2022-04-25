94f94bf3b1e5528484dc6e60a56ba76e5613e3e4
Hiroshi Kimura(m3) <hiroshi.kimura.0331@gmail.com>
2022-04-25 09:44:33 +0900

    function page/headline

--- src/typing_ex/typing.cljs
+++ src/typing_ex/typing.cljs
@@ -10,7 +10,7 @@
    [reagent.dom :as rdom]
    [typing-ex.plot :refer [plot]]))
 
-(def ^:private version "1.4.3")
+(def ^:private version "1.4.4")
 
 (defonce app-state (atom {:text "wait a little"
                           :answer ""
