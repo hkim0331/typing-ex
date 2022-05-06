d2127ec16da6c1827acf84423d58028761b75dac
Hiroshi Kimura <hiroshi.kimura.0331@gmail.com>
2022-05-06 20:48:06 +0900

    updated by projects.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 PORT=3023 \
     DATABASE_URL=jdbc:postgresql://db/l22?user=postgres \
-    lein run
- #    java -jar target/typing-ex-1.2.4-standalone.jar
+#    lein run
+    java -jar target/typing-ex-1.6.3-SNAPSHOT-standalone.jar
 
