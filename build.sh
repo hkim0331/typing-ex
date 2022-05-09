15bb4f9cadd0e562e773d458a70e9456350ea803
Hiroshi Kimura <hiroshi.kimura.0331@gmail.com>
2022-05-09 19:44:50 +0900

    update build.sh and run.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
-export TAOENSSO_TIMBRE_NS_PATTERN_EDN='{:deny #{"duct.database.sql.hikaricp" "duct.middleware.web"}}'
+export TAOENSSO_TIMBRE_MIN_LEVEL_EDN=':info'
+export TAOENSSO_TIMBRE_NS_PATTERN_EDN='{:deny #{"duct.middleware.web*"}}'
+#export TAOENSSO_TIMBRE_NS_PATTERN_EDN='{:deny #{"duct.database.sql.hikaricp" "duct.middleware.web"}}'
 yarn release
 lein uberjar
--- run.sh
+++ run.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
-PORT=3023 \
-  TAOENSSO_TIMBRE_MIN_LEVEL_EDN=':warn' \
-  DATABASE_URL=jdbc:postgresql://db/l22?user=postgres \
-  lein run
-#  java -jar target/typing-ex-1.6.3-SNAPSHOT-standalone.jar
+export PORT=3023
+export DATABASE_URL='jdbc:postgresql://db/l22?user=postgres'
+#  TAOENSSO_TIMBRE_MIN_LEVEL_EDN=':warn' \
+#  lein run
+java -jar target/typing-ex-1.6.5-SNAPSHOT-standalone.jar >> log/typing-ex.log
+
 
