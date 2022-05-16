2798eda4bd7da854a0c7c154f263307c40a08b9a
Hiroshi Kimura(m3) <hiroshi.kimura.0331@gmail.com>
2022-05-16 16:51:49 +0900

    updated by projects.sh

--- src/typing_ex/typing.cljs
+++ src/typing_ex/typing.cljs
@@ -77,6 +77,7 @@
              {:form-params
               {:pt (pt @app-state)
                :__anti-forgery-token (csrf-token)}}))
+
 ;; やや敗北。
 ;; go の戻りを待つ、あるいは検知できないか？
 (defn send-fetch-reset! []
