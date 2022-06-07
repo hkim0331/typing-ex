28e1a63d8dc14951ba22d6b987ad1039b8d4d927
Hiroshi Kimura <hiroshi.kimura.0331@gmail.com>
2022-06-07 21:27:39 +0900

    define table

--- src/typing_ex/typing.cljs
+++ src/typing_ex/typing.cljs
@@ -11,7 +11,7 @@
    [taoensso.timbre :as timbre]
    [typing-ex.plot :refer [bar-chart]]))
 
-(def ^:private version "1.8.4")
+(def ^:private version "1.9.0-SNAPSHOT")
 (def ^:private timeout 60)
 (def ^:private todays-limit 4)
 
