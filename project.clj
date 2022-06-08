c2461072dfc50b99bb583676197df24d19b2b0cf
Hiroshi Kimura <hiroshi.kimura.0331@gmail.com>
2022-06-08 16:26:00 +0900

    m2 1.9.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject typing-ex "1.9.0-SNAPSHOT"
+(defproject typing-ex "1.9.2"
   :description "typing exercises for literacy classes"
   :url "http://example.com/FIXME"
   :min-lein-version "2.0.0"
