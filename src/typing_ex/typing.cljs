6a9f25cb13dc6ae16bb233a85de41936abf40015
Hiroshi Kimura <hiroshi.kimura.0331@gmail.com>
2022-06-14 14:19:07 +0900

    read week-09

--- src/typing_ex/typing.cljs
+++ src/typing_ex/typing.cljs
@@ -11,7 +11,7 @@
    [taoensso.timbre :as timbre]
    [typing-ex.plot :refer [bar-chart]]))
 
-(def ^:private version "1.9.3.1")
+(def ^:private version "1.10.0")
 (def ^:private timeout 60)
 (def ^:private todays-limit 4)
 
