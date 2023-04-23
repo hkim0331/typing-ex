8e558c237648e3af1dd4d1c431846d4f89ab95ca
Hiroshi Kimura <hiroshi.kimura.0331@gmail.com>
2023-04-23 15:10:06 +0900

    total, max button

--- src/typing_ex/view/page.clj
+++ src/typing_ex/view/page.clj
@@ -9,7 +9,7 @@
    #_[taoensso.timbre :as timbre]
    [typing-ex.plot :refer [scatter]]))
 
-(def ^:private version "1.15.2")
+(def ^:private version "1.15.3")
 
 (defn page [& contents]
   [::response/ok
