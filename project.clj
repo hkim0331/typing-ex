ce4e4ae4b355a16ce6f279ff38aad398625d8a36
Hiroshi Kimura <hiroshi.kimura.0331@gmail.com>
2023-04-25 13:41:50 +0900

    stop using boxed

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject typing-ex "1.15.4"
+(defproject typing-ex "1.15.5"
   :description "typing exercises for literacy classes"
   :url "http://example.com/FIXME"
   :min-lein-version "2.0.0"
--- src/typing_ex/typing.cljs
+++ src/typing_ex/typing.cljs
@@ -10,7 +10,7 @@
    [reagent.dom :as rdom]
    [typing-ex.plot :refer [bar-chart]]))
 
-(def ^:private version "1.15.4")
+(def ^:private version "1.15.5")
 (def ^:private timeout 60)
 (def ^:private todays-limit 10)
 
