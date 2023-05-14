1330f48e043bc2ebf990f8a24f0156cff31dca86
Hiroshi Kimura <hiroshi.kimura.0331@gmail.com>
2023-05-14 14:01:14 +0900

    1.16.9

--- src/typing_ex/view/page.clj
+++ src/typing_ex/view/page.clj
@@ -9,7 +9,7 @@
    [ring.util.anti-forgery :refer [anti-forgery-field]]
    [typing-ex.plot :refer [scatter]]))
 
-(def ^:private version "1.16.8")
+(def ^:private version "1.16.9")
 
 (defn page [& contents]
   [::response/ok
