128bb16479e073e26d25fcb624bde3139d236c84
Hiroshi Kimura <hiroshi.kimura.0331@gmail.com>
2023-05-23 09:16:17 +0900

    return to /todays

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject typing-ex "1.18.9"
+(defproject typing-ex "1.18.10"
   :description "typing exercises for literacy classes"
   :url "http://example.com/FIXME"
   :min-lein-version "2.0.0"
