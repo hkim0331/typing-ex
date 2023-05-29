1a6e213635abd7b8ffd5c3dbcb2372c5d658095c
Hiroshi Kimura <hiroshi.kimura.0331@gmail.com>
2023-05-29 19:44:32 +0900

    release 1.19.4

--- src/typing_ex/typing.cljs
+++ src/typing_ex/typing.cljs
@@ -255,11 +255,14 @@ a hat. It was supposed to be a boa constrictor digesting elephant.
                      js/parseInt)
             now (.now js/Date.)
             diff  (- now last)]
+        (<! (http/post
+             "/restarts"
+             {:form-params {:__anti-forgery-token (csrf-token)}}))
          ;; 20 seconds
         (when (< diff 20000)
           (js/alert (str "むずいのでも練習しなくちゃ。" diff))
           (busy-wait 10000))))
-  (go (<! (http/post
+  #_(go (<! (http/post
            "/restarts"
            {:form-params {:__anti-forgery-token (csrf-token)}}))))
 
