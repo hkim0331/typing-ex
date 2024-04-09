5fa987606c7e1fbfc7648dfd93a83ddb50b5c907
Hiroshi Kimura <hiroshi.kimura.0331@gmail.com>
2024-04-09 10:02:30 +0900

    release 2.0.781, docker incomlete

--- src/typing_ex/view/page.clj
+++ src/typing_ex/view/page.clj
@@ -11,7 +11,7 @@
    [typing-ex.plot :refer [scatter]]
    #_[clojure.test :as t]))
 
-(def ^:private version "1.23.778")
+(def ^:private version "2.0.781")
 
 ;--------------------------------
 ;; FIXME
