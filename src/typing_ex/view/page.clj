826b3408d4578ef76814010a2ebde005b8201fb7
Hiroshi Kimura <hiroshi.kimura.0331@gmail.com>
2024-04-21 09:19:31 +0900

    try off line typing-ex on m1

--- src/typing_ex/view/page.clj
+++ src/typing_ex/view/page.clj
@@ -11,7 +11,7 @@
    [typing-ex.plot :refer [scatter]]
    #_[clojure.test :as t]))
 
-(def ^:private version "v2.4.829")
+(def ^:private version "v2.5.836")
 
 ;--------------------------------
 ;; FIXME
