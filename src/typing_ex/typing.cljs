da7810ce99399df97506af6ceb4e27077bc5b854
Hiroshi Kimura <hiroshi.kimura.0331@gmail.com>
2024-04-28 15:04:34 +0900

    with redis, carmine.

--- src/typing_ex/typing.cljs
+++ src/typing_ex/typing.cljs
@@ -10,7 +10,7 @@
    [typing-ex.plot :refer [bar-chart]]))
 
 
-(def ^:private version "v2.6-SNAPSHOT-2")
+(def ^:private version "v2.6.870")
 
 (def ^:private timeout 60)
 (def ^:private todays-limit 10)
--- src/typing_ex/view/page.clj
+++ src/typing_ex/view/page.clj
@@ -11,7 +11,7 @@
    [typing-ex.plot :refer [scatter]]
    #_[clojure.test :as t]))
 
-(def ^:private version "v2.6-SNAPSHOT-2")
+(def ^:private version "v2.6.870")
 
 ;--------------------------------
 ;; FIXME
