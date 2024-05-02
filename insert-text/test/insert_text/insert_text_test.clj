7871bee4ec631dcadf066471dcdcad326a3d2f47
Hiroshi Kimura <hiroshi.kimura.0331@gmail.com>
2024-05-02 15:20:04 +0900

    added insert-text

---
+++ insert-text/test/insert_text/insert_text_test.clj
@@ -0,0 +1,7 @@
+(ns insert-text.insert-text-test
+  (:require [clojure.test :refer :all]
+            [insert-text.cire :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
