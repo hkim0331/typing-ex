14e77fc980e4ae964354901e215ae4b81c45e61e
Hiroshi Kimura <hiroshi.kimura.0331@gmail.com>
2024-06-04 22:08:11 +0900

    v2.11.945

--- src/typing_ex/typing.cljs
+++ src/typing_ex/typing.cljs
@@ -9,7 +9,7 @@
    [reagent.dom :as rdom]
    [typing-ex.plot :refer [bar-chart]]))
 
-(def ^:private version "v2.10.932")
+(def ^:private version "v2.11.945")
 
 (def ^:private timeout 60)
 (def ^:private todays-limit 10)
