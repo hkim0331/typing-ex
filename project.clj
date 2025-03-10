18d0f01e5b8d02be4d6e7e8afb6e569bc0782a7e
Hiroshi Kimura <hiroshi.kimura.0331@gmail.com>
2025-03-10 16:10:33 +0900

    next.jdbc and postgres

--- project.clj
+++ project.clj
@@ -6,7 +6,7 @@
                  [buddy/buddy-hashers "2.0.167"]
                  [cheshire/cheshire "5.13.0"]
                  [clojure.java-time/clojure.java-time "1.4.2"]
-                 [com.github.seancorfield/next.jdbc "1.3.939"]
+                 [com.github.seancorfield/next.jdbc "1.3.1002"] ; 939
                  [dev.weavejester/medley "1.8.1"]
                  [duct/core "0.8.1"]
                  [duct/module.ataraxy "0.3.0"]
@@ -18,9 +18,10 @@
                  [hiccup "1.0.5"]
                  [com.taoensso/carmine "3.4.1"]
                  [com.taoensso/encore "3.121.0"]
-                 [com.taoensso/telemere "1.0.0-beta25"]
+                 ;; [com.taoensso/telemere "1.0.0-beta25"]
                  [org.clojure/clojure "1.12.0"]
-                 [org.postgresql/postgresql "42.7.4"]]
+                 [org.postgresql/postgresql "42.7.5"];; 42.7.4
+                 ]
   :plugins [[duct/lein-duct "0.12.3"]]
   :main ^:skip-aot typing-ex.main
   :resource-paths ["resources" "target/resources"]
